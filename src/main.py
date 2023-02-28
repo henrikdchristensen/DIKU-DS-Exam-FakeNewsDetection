03e86b451330fffbc3157fa06a610e51ca6cb79c
a-lundsgaard <72150030+a-lundsgaard@users.noreply.github.com>
2023-02-28 15:32:10 +0100

    updates

--- src/main.py
+++ src/main.py
@@ -10,13 +10,26 @@ os.chdir("..")
 
 
 def main():
-    df = pd.read_csv(file, usecols=['content'])
     pd.set_option('display.max_colwidth', None)
-    df = ps.clean_text(df)
-    df = ps.tokenize_text(df)
-    print(df.loc[1])
+    df = pd.read_csv(file)
+    # df = ps.clean_text(df)
+    # df = ps.tokenize_text(df)
+    # print(ps.Exploration.get_stopwords(df, freq_low = 5e-5, freq_high = 1e-3, print_stopwords_info = True))
 
 
+    url_count = ps.Exploration.countItems(df)
+    source_dist = ps.Exploration.sourceDistribution(df)
+
+    print(source_dist)
+    # date_count = ps.Exploration.countDates(df)
+    # number_count = ps.Exploration.countNumbers(df)
+
+    # print("URLs: ", url_count, "Dates: ", date_count, "Numbers: ", number_count)
+
+    print("URLs: ", url_count)
+
+
+# def exploreData() 
 
 
 if __name__ == '__main__':
