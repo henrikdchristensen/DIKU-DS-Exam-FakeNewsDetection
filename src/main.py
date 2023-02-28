20c620283fc7455293208f45c96baac4ad7011d2
mads309f <2mads.v.jensen@gmail.com>
2023-02-28 13:19:42 +0100

    errors fixed

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import preprocessing
+import Preprocessing as ps
 import os
 
 #file = "https://raw.githubusercontent.com/several27/FakeNewsCorpus/master/news_sample.csv"
@@ -12,9 +12,11 @@ os.chdir("..")
 def main():
     df = pd.read_csv(file, usecols=['content'])
     pd.set_option('display.max_colwidth', None)
-    df = preprocessing.clean_text(df)
-    df = preprocessing.tokenize_text(df)
+    df = ps.clean_text(df)
+    df = ps.tokenize_text(df)
     print(df.loc[1])
+
+
 
 
 if __name__ == '__main__':
