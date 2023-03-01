aa1f04274e48275315346ec2f332989a07b870a6
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-02 00:03:55 +0100

    fix exception, if content contains '/r'

--- src/filehandling.py
+++ src/filehandling.py
@@ -1,5 +1,3 @@
-# file = "https://raw.githubusercontent.com/several27/FakeNewsCorpus/master/news_sample.csv"
-# file = "datasets/news_sample.csv"
 import numpy as np
 import csv
 import os
@@ -7,18 +5,19 @@ import pandas as pd
 from tqdm import tqdm
 
 #csv_file = "datasets/news_sample.csv"
+# https://raw.githubusercontent.com/several27/FakeNewsCorpus/master/news_sample.csv
 csv_file = "datasets/news_cleaned_2018_02_13.csv"
 hdf_file = 'data.h5'
 train_file = 'train.h5'
 vali_file = 'vali.h5'
 test_file = 'test.h5'
-CHUNK_SIZE = 500
+CHUNK_SIZE = 300
 COL_NAMES = ['x', 'id', 'domain', 'type', 'url', 'content', 'scraped_at', 'inserted_at', 'updated_at', 'title',
              'authors', 'keywords', 'meta_keywords', 'meta_description', 'tags', 'summary', 'source']
 COL_SIZES = {'x': 300, 'id': 150, 'domain': 50, 'type': 50, 'url': 2000, 'content': 200000, 'scraped_at': 100, 'inserted_at': 100, 'updated_at': 100,
              'title': 400, 'authors': 800, 'keywords': 5, 'meta_keywords': 40000, 'meta_description': 15000, 'tags': 30000, 'summary': 5, 'source': 5}
 ROWS = 216212648
-
+#TODO: 
 
 # Set the current directory one level up:
 os.chdir("..")
@@ -47,8 +46,8 @@ def csv_to_hdf(csv_filename: str, hdf_filename: str, cols_sizes=COL_SIZES, chunk
         os.remove(hdf_filename)
     # Read csv as chunks and append to hdf file:
     with pd.HDFStore(hdf_filename, complib='blosc', complevel=9) as store:
-        for chunk in tqdm(pd.read_csv(csv_filename, encoding='utf-8', dtype=str, chunksize=chunk_size, names=col_names),
-                          desc='csv to hdf format', total=ROWS/chunk_size):
+        for chunk in tqdm(pd.read_csv(csv_filename, encoding='utf-8', dtype=str, chunksize=chunk_size, names=col_names, lineterminator='\n'),
+                          desc='csv to hdf format', total=int(ROWS/chunk_size)):
             store.append(key='data', value=chunk,
                          index=False, min_itemsize=cols_sizes)
 
@@ -63,12 +62,14 @@ def read_hdf(filename: str, startIdx=0, stopIdx=0, columns_to_return=None):
 
 
 def create_train_vali_and_test_sets(split, data_filename: str, train_filename: str, vali_filename: str, test_filename: str, cols_sizes=COL_SIZES):
+    # Remove exiting hdf files:
     if os.path.exists(train_filename):
         os.remove(train_filename)
     if os.path.exists(vali_filename):
         os.remove(vali_filename)
     if os.path.exists(test_filename):
         os.remove(test_filename)
+    # Run through data file and match each row with the corresponding shuffled array:
     with pd.HDFStore(train_filename, complib='blosc', complevel=9) as train,\
             pd.HDFStore(vali_filename, complib='blosc', complevel=9) as vali,\
             pd.HDFStore(test_filename, complib='blosc', complevel=9) as test:
@@ -107,7 +108,7 @@ def create_randomly_split_array(size: int):
 
 # print(get_csv_header(csv_file))
 split = create_randomly_split_array(ROWS)
-csv_to_hdf(csv_file, hdf_file, colssizes)
+csv_to_hdf(csv_file, hdf_file)
 
 create_train_vali_and_test_sets(split, data_filename=hdf_file, train_filename=train_file,
                                 vali_filename=vali_file, test_filename=test_file)
