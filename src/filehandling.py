34bc5b73f21ac4a6a3c2298cd5bf9776bf2d1790
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-02 16:07:37 +0100

    cleanup

--- src/filehandling.py
+++ src/filehandling.py
@@ -41,17 +41,18 @@ def remove_file(filename: str):
         os.remove(filename)
 
 
+def create_dataset(name: str):
+    arr = np.zeros((1, COLS), dtype=object)
+    arr[0] = ["" for x in range(COLS)]
+    return store.create_dataset(name, data=arr, maxshape=(
+        None, COLS), dtype=h5py.string_dtype(encoding='utf-8'))
+
+
 def csv_to_hdf(csv_filename: str, hdf_filename: str):
-
     remove_file(hdf_filename)
-
     # Read csv as chunks so we don't run out of memory and append to hdf file:
     with h5py.File(hdf_filename, 'w') as store:
-        # Create dataset with one dummy string array:
-        arr = np.zeros((1, COLS), dtype=object)
-        arr[0] = ["" for x in range(COLS)]
-        dset = store.create_dataset('data', data=arr, maxshape=(
-            None, COLS), dtype=h5py.string_dtype(encoding='utf-8'))
+        dset = create_dataset('data')
         # Get and set the header row:
         with open(csv_filename, encoding='utf-8') as f:
             dset[0] = next(csv.reader(f))
@@ -74,29 +75,20 @@ def create_train_vali_and_test_sets(split, data_filename: str, train_filename: s
     remove_file(train_filename)
     remove_file(vali_filename)
     remove_file(test_filename)
+
     # Run through data file and match each row with the corresponding shuffled array:
     with h5py.File(data_filename, 'r', ) as data,\
             h5py.File(train_filename, 'w') as train,\
             h5py.File(vali_filename, 'w') as vali,\
             h5py.File(test_filename, 'w') as test:
-
         data = data['data']
-
-        arr = np.zeros((1, COLS), dtype=object)
-        arr[0] = ["" for x in range(COLS)]
-        trainset = train.create_dataset('train', data=arr, maxshape=(
-            None, COLS), dtype=h5py.string_dtype(encoding='utf-8'))
-        valiset = vali.create_dataset('vali', data=arr, maxshape=(
-            None, COLS), dtype=h5py.string_dtype(encoding='utf-8'))
-        testset = test.create_dataset('test', data=arr, maxshape=(
-            None, COLS), dtype=h5py.string_dtype(encoding='utf-8'))
-
+        trainset = create_dataset('train')
+        valiset = create_dataset('vali')
+        testset = create_dataset('test')
         # Set header row:
         trainset[0] = valiset[0] = testset[0] = data[0, ]
-
         # Loop through split array and append data to the right dataset:
-        for i in tqdm(range(0, len(split)),
-                      desc='split dataset', total=len(split)):
+        for i in tqdm(range(0, len(split)), desc='split dataset', total=len(split)):
             match split[i]:
                 case 0:
                     trainset.resize((trainset.shape[0]+1, COLS))
@@ -116,18 +108,14 @@ def create_randomly_split_array(size: int):
 
     # Create a numpy array of the given size and set all to zeroes
     arr = np.zeros(size, dtype=int)
-
     # Determine the indices for the three splits
     split1 = int(size * 0.8)
     split2 = int(size * 0.9)
-
     # Set the other two's values
     arr[split1:split2] = 1
     arr[split2:] = 2
-
     # Shuffle the indexes of the array
     np.random.shuffle(arr)
-
     return arr
 
 
