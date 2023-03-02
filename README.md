0d23dabe51ac0bc72e25ab99e1aac392dac8409e
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-02 20:27:58 +0100

    fix train_vali_test function and update gitignore

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FakeNews
+
+# Access Data files:
+
+http://gofile.me/6vSyS/X1xgrq6cI
--- src/filehandling.py
+++ src/filehandling.py
@@ -7,7 +7,7 @@ import os
 import pandas as pd
 from tqdm import tqdm
 
-
+TQDM_COLOR = 'magenta'
 # https://raw.githubusercontent.com/several27/FakeNewsCorpus/master/news_sample.csv
 csv_file = "datasets/news_cleaned_2018_02_13.csv"
 ROWS = 8528956
@@ -16,18 +16,18 @@ COLS = 17
 #ROWS = 250
 #COLS = 16
 hdf_file = 'data.h5'
-train_file = 'train.h5'
-vali_file = 'vali.h5'
-test_file = 'test.h5'
+train_file = 'datasets/train.h5'
+vali_file = 'datasets/vali.h5'
+test_file = 'datasets/test.h5'
 CHUNK_SIZE = 10
 
 # Set the current directory one level up:
 os.chdir("..")
 
 
-def num_rows_and_cols_csv(_csv_file: str):
+def num_rows_and_cols_csv(csv_file: str):
     # On MacOS: Use another command:
-    cmd = 'Import-Csv ".\\' + _csv_file + '" | Measure-Object'
+    cmd = 'Import-Csv ".\\' + csv_file + '" | Measure-Object'
     result = subprocess.run(
         ['powershell', '-Command', cmd], capture_output=True, text=True)
     if result.returncode == 0:
@@ -41,15 +41,16 @@ def remove_file(filename: str):
         os.remove(filename)
 
 
-# def create_dataset(filename: str, dataset_name: str):
+def empty_string_array():
+    arr = np.zeros((1, COLS), dtype=object)
+    arr[0] = ["" for x in range(COLS)]
+    return arr
 
 
 def csv_to_hdf(csv_filename: str, hdf_filename: str):
     remove_file(hdf_filename)
-    arr = np.zeros((1, COLS), dtype=object)
-    arr[0] = ["" for x in range(COLS)]
     with h5py.File(hdf_filename, 'w') as store:
-        dset = store.create_dataset('data', data=arr, maxshape=(
+        dset = store.create_dataset('data', data=empty_string_array(), maxshape=(
             None, COLS), dtype=h5py.string_dtype(encoding='utf-8'))
         # Get and set the header row:
         with open(csv_filename, encoding='utf-8') as f:
@@ -57,15 +58,20 @@ def csv_to_hdf(csv_filename: str, hdf_filename: str):
             # Read the rest of the rows and assign to dataset:
         rows = 1
         for c in tqdm(pd.read_csv(csv_filename, encoding='utf-8', dtype=str, chunksize=CHUNK_SIZE),
-                      desc='.csv to .h5', total=int(ROWS/CHUNK_SIZE), unit='rows', unit_scale=CHUNK_SIZE, colour='magenta'):
+                      desc='.csv to .h5', total=int(ROWS/CHUNK_SIZE), unit='rows', unit_scale=CHUNK_SIZE, colour=TQDM_COLOR):
             rows += len(c)
             dset.resize((rows, COLS))
             dset[-len(c):] = c.astype(str).values
 
 
-def read_hdf(filename: str, startIdx=0, stopIdx=0):
+def read_hdf_rows(filename: str, idx=0, num=0):
     with h5py.File(filename, 'r') as f:
-        return f['data'][startIdx:stopIdx+1, ]
+        return f['data'][idx:idx+num, :]
+
+
+def read_hdf_cols(filename: str, idx=0, num=0):
+    with h5py.File(filename, 'r') as f:
+        return f['data'][:, idx:idx+num]
 
 
 def create_train_vali_and_test_sets(split, data_filename: str, train_filename: str, vali_filename: str, test_filename: str):
@@ -73,13 +79,13 @@ def create_train_vali_and_test_sets(split, data_filename: str, train_filename: s
     remove_file(train_filename)
     remove_file(vali_filename)
     remove_file(test_filename)
-
     # Run through data file and match each row with the corresponding shuffled array:
     with h5py.File(data_filename, 'r', ) as data,\
-            h5py.File(train_filename, 'r', ) as train,\
-            h5py.File(vali_filename, 'r', ) as vali,\
-            h5py.File(test_filename, 'r', ) as test:
+            h5py.File(train_filename, 'w', ) as train,\
+            h5py.File(vali_filename, 'w', ) as vali,\
+            h5py.File(test_filename, 'w', ) as test:
         data = data['data']
+        arr = empty_string_array()
         trainset = train.create_dataset('data', data=arr, maxshape=(
             None, COLS), dtype=h5py.string_dtype(encoding='utf-8'))
         valiset = vali.create_dataset('data', data=arr, maxshape=(
@@ -89,7 +95,7 @@ def create_train_vali_and_test_sets(split, data_filename: str, train_filename: s
         # Set header row:
         trainset[0] = valiset[0] = testset[0] = data[0, ]
         # Loop through split array and append data to the right dataset:
-        for i in tqdm(range(0, len(split)), desc='split dataset', total=len(split), unit='rows', colour='magenta'):
+        for i in tqdm(range(0, len(split)), desc='split dataset', total=len(split), unit='rows', colour=TQDM_COLOR):
             match split[i]:
                 case 0:
                     trainset.resize((trainset.shape[0]+1, COLS))
@@ -123,9 +129,9 @@ def create_randomly_split_array(size: int):
 # print(num_rows_and_cols_csv(csv_file))
 #csv_to_hdf(csv_file, hdf_file)
 
-rows = read_hdf(hdf_file, startIdx=0, stopIdx=256)
+rows = read_hdf_rows(filename=hdf_file, idx=0, num=256)
 print(rows[0, 2])
 
-split = create_randomly_split_array(ROWS)
+split = create_randomly_split_array(size=ROWS)
 create_train_vali_and_test_sets(split, data_filename=hdf_file,
                                 train_filename=train_file, vali_filename=vali_file, test_filename=test_file)
