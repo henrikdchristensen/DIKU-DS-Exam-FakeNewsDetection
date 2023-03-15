c216397a8b88399820f0edb8b684c1416bf40436
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-15 06:57:48 +0100

    fix all needed tests

--- tests/test_filehandling.py
+++ tests/test_filehandling.py
@@ -1,21 +1,35 @@
 import numpy as np
 import src.filehandling as fh
 
-FILENAME = "../datasets/sample/news_sample.csv"
-ROWS = 250
+DATA_FILENAME = "files/news_sample.csv"
+TRAIN_FILENAME = "files/train.csv"
+VALI_FILENAME = "files/vali.csv"
+TEST_FILENAME = "files/test.csv"
+OLD_SIZE = 250
+NEW_SIZE = 200
 ROWS_PR_ITERATION = 200000
+SPLIT = (0.8, 0.1, 0.1)
 
 
 def test_read_rows():
-    rows = fh.read_rows(filename=FILENAME, idx=2, num=2)
+    rows = fh.read_rows(filename=DATA_FILENAME, idx=2, num=2)
     assert rows.iloc[0, 3] == 'unreliable'
     assert rows.iloc[1, 2] == 'awm.com'
 
 
 def test_create_randomly_split_array():
-    new_size = 100
-    arr = fh.create_randomly_split_array(old_size=ROWS, new_size=new_size, split=(0.8, 0.1, 0.1))
-    assert len(arr) == ROWS
-    assert np.sum(arr == fh.Set.TRAIN) == new_size*0.8
-    assert np.sum(arr == fh.Set.VALI) == new_size*0.1
-    assert np.sum(arr == fh.Set.TEST) == new_size*0.1
+    arr = fh.create_randomly_split_array(old_size=OLD_SIZE, new_size=NEW_SIZE, split=SPLIT)
+    assert len(arr) == OLD_SIZE
+    assert np.sum(arr == fh.Set.TRAIN) == NEW_SIZE*0.8
+    assert np.sum(arr == fh.Set.VALI) == NEW_SIZE*0.1
+    assert np.sum(arr == fh.Set.TEST) == NEW_SIZE*0.1
+    
+    
+def test_number_of_rows():
+    assert fh.number_of_rows(filename=DATA_FILENAME, rows_pr_iteration=ROWS_PR_ITERATION) == OLD_SIZE
+    
+def test_create_train_vali_and_test_sets():
+    fh.create_train_vali_and_test_sets(old_size=OLD_SIZE, new_size=NEW_SIZE, split=SPLIT, data_filename=DATA_FILENAME, train_filename=TRAIN_FILENAME, vali_filename=VALI_FILENAME, test_filename=TEST_FILENAME, rows_pr_iteration=ROWS_PR_ITERATION)
+    assert fh.number_of_rows(filename=TRAIN_FILENAME, rows_pr_iteration=ROWS_PR_ITERATION) == NEW_SIZE*0.8
+    assert fh.number_of_rows(filename=VALI_FILENAME, rows_pr_iteration=ROWS_PR_ITERATION) == NEW_SIZE*0.1
+    assert fh.number_of_rows(filename=TEST_FILENAME, rows_pr_iteration=ROWS_PR_ITERATION) == NEW_SIZE*0.1
