09192fa198c6c574d1571ae8d503e58276a6360d
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-19 13:54:51 +0100

    modify functions to balance dataset, NOT finished yet

--- src/filehandling.py
+++ src/filehandling.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from collections import Counter
+from typing import Dict, Tuple
 from enum import IntEnum
 import shutil
 import numpy as np
@@ -8,6 +9,8 @@ import pandas as pd
 from tqdm import tqdm
 
 TQDM_COLOR = 'magenta'
+types = {'fake': 1, 'conspiracy': 2, 'junksci': 3, 'hate': 4, 'unreliable': 5,
+         'bias': 6, 'satire': 7, 'state': 8, 'reliable': 9, 'clickbait': 10, 'political': 11}
 
 
 def remove_file(filename: str):
@@ -60,6 +63,36 @@ def create_randomly_array(old_size: int, new_size: int) -> np.ndarray:
     return arr
 
 
+types = {'fake': 1, 'conspiracy': 2, 'junksci': 3, 'hate': 4, 'unreliable': 5,
+         'bias': 6, 'satire': 7, 'state': 8, 'reliable': 9, 'clickbait': 10, 'political': 11}
+
+
+def create_randomly_balanced_array(types_list: list, new_size: int, threshold: float = 0.05) -> np.ndarray:
+    # Calculate the counts of each type in the original array
+    type_counts = np.bincount(types_list, minlength=len(types))
+    print(type_counts)
+    # Calculate the target count for each type in the new array
+    target_counts = np.around(
+        type_counts.sum() * np.array(list(types.values())) / sum(types.values()))
+    # Calculate the maximum allowed deviation from the target count, based on the threshold
+    max_deviation = np.around(threshold * target_counts.sum())
+    # Generate the new array
+    new_array = np.zeros(len(types), dtype=bool)
+    i = 0
+    while i < new_size:
+        # Select a random type index to add to the new array
+        type_index = np.random.randint(len(types))
+        # Check if adding this type would exceed the maximum deviation from the target count
+        if abs(target_counts[type_index] - np.count_nonzero(new_array == type_index)) > max_deviation:
+            continue
+        # Add the type to the new array
+        new_array[type_index] = True
+        i += 1
+    # Shuffle the indexes of the new array and return
+    new_array = np.random.permutation(new_array)
+    return new_array
+
+
 def csv_split(filename: str, dirname: str = 'csv-chunks', rows_pr_iteration: int = 20000, padding: int = 4):
     # Get header row:
     with open(filename, encoding='utf-8') as f:
@@ -74,26 +107,40 @@ def csv_split(filename: str, dirname: str = 'csv-chunks', rows_pr_iteration: int
             f'{dirname}/{i+1:0{padding}}.csv', index=False)
 
 
-def remove_unwanted_rows(filename: str, new_filename: str, rows_pr_iteration: int = 20000) -> int:
-    type = ['fake', 'conspiracy', 'junksci', 'hate', 'unreliable',
-            'bias', 'satire', 'state', 'reliable', 'clickbait', 'political']
+def data_preparation(filename: str, new_filename: str, rows_pr_iteration: int = 20000) -> Tuple[int, np.ndarray]:
     with open(filename, encoding='utf-8') as f:
         colnames = pd.DataFrame(columns=next(csv.reader(f)))
     colnames.to_csv(new_filename, mode='w')
     original_rows = 0
     retained_rows = 0
+    type_arr = np.empty(0, dtype=int)
     with pd.read_csv(filename, encoding='utf-8', chunksize=rows_pr_iteration, lineterminator='\n') as reader:
         for chunk in tqdm(reader, desc='remove missing/false values', unit='rows encountered', unit_scale=rows_pr_iteration, colour=TQDM_COLOR):
             original_rows += chunk.shape[0]
             # Drop rows with empty content column:
             chunk = chunk.dropna(subset=['content'])
             # Remove rows where type is not one of the specified values:
-            chunk = chunk[chunk['type'].isin(type)]
+            chunk = chunk[chunk['type'].isin(types)]
             retained_rows += chunk.shape[0]
             chunk.to_csv(new_filename, mode='a', header=None)
+            # Append the 'type' column to the type_array
+            # Append the value of the 'type' column to the type_array
+            type_arr = np.append(type_arr, chunk['type'].map(types))
+            #type_arr.extend([types[t] for t in chunk['type'].tolist()])
     print(
-        f"removed rows: {original_rows-retained_rows}\n(original rows: {original_rows}, retained rows: {retained_rows})")
-    return retained_rows
+        f"Removed rows: {original_rows-retained_rows}\n(original rows: {original_rows}, retained rows: {retained_rows})")
+    print("\nType counts:")
+    unique, counts = np.unique(type_arr, return_counts=True)
+    for i in range(len(unique)):
+        print(
+            f"{list(types.keys())[list(types.values()).index(unique[i])]}: {counts[i]}")
+    missing_types = set(types.values()) - set(unique)
+    if len(missing_types) > 0:
+        print("\nMissing types:")
+        for t in missing_types:
+            print(
+                f"{list(types.keys())[list(types.values()).index(t)]}")
+    return (retained_rows, type_arr)
 
 
 def create_train_vali_and_test_sets(old_size: int, new_size: int, split: Tuple[float, float, float], data_filename: str, train_filename: str, vali_filename: str, test_filename: str, rows_pr_iteration: int = 20000):
@@ -172,8 +219,8 @@ def run(sample: bool = True, rows_cleaned: bool = False, rows_pr_iteration: int 
         path = "../datasets/large/"
     create_directory(path)
     if not rows_cleaned:
-        old_size = remove_unwanted_rows(filename=path+"raw.csv",
-                                        new_filename=path+"cleaned.csv", rows_pr_iteration=rows_pr_iteration)
+        (old_size, types) = data_preparation(filename=path+"raw.csv",
+                                             new_filename=path+"cleaned.csv", rows_pr_iteration=rows_pr_iteration)
     if not split:
         create_dataset(old_size=old_size, new_size=new_size, old_filename=path+"cleaned.csv",
                        new_filename=path+"dataset.csv", rows_pr_iteration=rows_pr_iteration)
@@ -183,5 +230,7 @@ def run(sample: bool = True, rows_cleaned: bool = False, rows_pr_iteration: int 
 
 
 if __name__ == '__main__':
-    run(sample=True, rows_cleaned=False,
-        rows_pr_iteration=ROWS_PR_ITERATION, split=False, new_size=200)
+    # run(sample=True, rows_cleaned=False,
+    #     rows_pr_iteration=ROWS_PR_ITERATION, split=False, new_size=200)
+    data_preparation(filename="../datasets/sample/raw.csv",
+                     new_filename="../datasets/sample/cleaned.csv", rows_pr_iteration=ROWS_PR_ITERATION)
