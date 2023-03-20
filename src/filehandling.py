4dd37f17ba6c17c2b01f32991f3aba90f35a35b0
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-20 22:03:14 +0100

    process the files as temporary splitted csv files.

--- src/filehandling.py
+++ src/filehandling.py
@@ -5,12 +5,15 @@ import csv
 import os
 import pandas as pd
 from tqdm import tqdm
+import math
 
 TQDM_COLOR = 'magenta'
 TYPES_DICT = ['fake', 'conspiracy', 'junksci', 'hate', 'unreliable', 'bias', 
               'satire', 'state', 'reliable', 'clickbait', 'political']
 SAMPLE = False
 ROWS_PR_ITERATION = 20000
+FILE_SIZE = 50000
+PADDING = 3
 
 def remove_file(filename: str):
     if os.path.exists(filename):
@@ -31,7 +34,7 @@ def create_random_array(size:int) -> np.ndarray:
     np.random.shuffle(arr)
     return arr
 
-def csv_split(filename: str, dirname: str = 'csv-chunks', rows_pr_iteration: int = 20000, padding: int = 4):
+def csv_split(filename: str, dirname: str, file_size: int = 20000, padding: int = 3):
     # Get and set header row:
     with open(filename, encoding='utf-8') as f:
         colnames = pd.DataFrame(columns=next(csv.reader(f)))
@@ -39,26 +42,30 @@ def csv_split(filename: str, dirname: str = 'csv-chunks', rows_pr_iteration: int
     remove_directory(dirname)
     create_directory(dirname)
     # Add each chunk to a file in the directory:
-    for i, c in tqdm(enumerate(pd.read_csv(filename, encoding='utf-8', chunksize=rows_pr_iteration, lineterminator='\n')),
-                     desc='csv split', unit='splits', colour=TQDM_COLOR):
-        pd.concat([colnames, c], ignore_index=True).to_csv(
-            f'{dirname}/{i+1:0{padding}}.csv', index=False)
+    for i, c in tqdm(enumerate(pd.read_csv(filename, encoding='utf-8', chunksize=file_size, lineterminator='\n')),
+                     desc='csv splitting', unit='splits', colour=TQDM_COLOR):
+        pd.concat([colnames, c], ignore_index=True).to_csv(f'{dirname}/{i+1:0{padding}}.csv', index=False)
 
 def remove_unwanted(filename: str, new_filename: str, rows_pr_iteration: int = 20000) -> int:
+    # Write the header row to the new file:
     with open(filename, encoding='utf-8') as f:
         colnames = pd.DataFrame(columns=next(csv.reader(f)))
+    # Remove first coloumn (unnamed), which is not used:
+    colnames.drop('', inplace=True, axis=1)
     colnames.to_csv(new_filename, mode='w', index=False)
-    original_rows = 0
-    retained_rows = 0
+    original_rows = retained_rows = 0
     with pd.read_csv(filename, encoding='utf-8', chunksize=rows_pr_iteration, lineterminator='\n') as reader:
-        for chunk in tqdm(reader, desc='remove missing/incorrect values', unit='rows', unit_scale=rows_pr_iteration, colour=TQDM_COLOR):
+        for chunk in tqdm(reader, desc='remove unwanted rows', unit='rows encountered', unit_scale=rows_pr_iteration, colour=TQDM_COLOR):
             original_rows += chunk.shape[0]
             # Drop rows with empty content column:
             chunk = chunk.dropna(subset=['content'])
             # Remove rows where type is not one of the specified values:
             chunk = chunk[chunk['type'].isin(TYPES_DICT)]
             # Set unique index for each row:
-            chunk.iloc[:,0] = chunk.iloc[:,0].reset_index(drop=True).index + retained_rows
+            chunk['id'] = chunk['id'].reset_index(drop=True).index + retained_rows
+            # Remove index column:
+            chunk = chunk.reset_index(drop=True)
+            # Append processed chunk to new file:
             chunk.to_csv(new_filename, mode='a', header=None, index=False)
             retained_rows += chunk.shape[0]
     print(f"Removed rows: {original_rows-retained_rows}\n(rows before: {original_rows}, rows after (retained): {retained_rows})")
@@ -68,19 +75,31 @@ def read_rows(filename: str, idx: int, num: int = 1) -> int:
     return pd.read_csv(filename, encoding='utf-8', lineterminator='\n', skiprows=idx, nrows=num)
 
 def create_dataset(size:int, old_filename: str, new_filename: str):
+    temp_dir = "temp"
+    # Split the old file into chunks for faster processing:
+    csv_split(filename=old_filename, dirname=temp_dir, file_size=FILE_SIZE, padding=PADDING)
+    # Create a random array of the given size:
     random_arr = create_random_array(size=size)
     # Write the header row to the new files:
-    with open(old_filename, encoding='utf-8') as f:
+    with open(f'{temp_dir}/{1:0{PADDING}}.csv', encoding='utf-8') as f:
         colnames = pd.DataFrame(columns=next(csv.reader(f)))
     colnames.to_csv(new_filename, mode='w', index=False)
     # Loop through cleaned dataset and take out rows corresponding to randomly created array:
-    for index in tqdm(random_arr, desc='creating dataset', unit='rows', colour=TQDM_COLOR):
-        read_rows(old_filename, index, 1).to_csv(new_filename, mode='a', header=None, index=False)
+    for i in tqdm(random_arr, desc='creating dataset', unit='rows encountered', colour=TQDM_COLOR):
+        # Find the chunk file:
+        file_num = math.ceil((i+1)/FILE_SIZE)
+        # Find the row index of the row to be read:
+        row_idx = i-(file_num-1)*FILE_SIZE
+        # Read the row and append it to the new file:
+        read_rows(f'{temp_dir}/{file_num:0{PADDING}}.csv', row_idx, 1).to_csv(new_filename, mode='a', header=None, index=False)
+    # Remove the temporary directory:
+    #remove_directory(temp_dir)#TODO
 
 def run(sample: bool, rows_pr_iteration: int = ROWS_PR_ITERATION):
     path = "../datasets/sample/" if sample else "../datasets/large/"
     size = remove_unwanted(filename=path+"raw.csv", new_filename=path+"cleaned.csv", rows_pr_iteration=rows_pr_iteration)
     create_dataset(size=size, old_filename=path+"cleaned.csv", new_filename=path+"dataset.csv")
+    
 
 if __name__ == '__main__':
     run(sample=SAMPLE, rows_pr_iteration=ROWS_PR_ITERATION)
