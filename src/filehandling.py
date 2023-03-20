ed968f8923b5e8120c9fd44cf2a8a3770821efbc
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-20 14:56:58 +0100

    breaking change for filehandling

--- src/filehandling.py
+++ src/filehandling.py
@@ -14,7 +14,7 @@ TYPES_DICT = {'fake': 1, 'conspiracy': 2, 'junksci': 3, 'hate': 4, 'unreliable':
 
 #ROWS_LARGE = 7273069
 #ROWS_SAMPLE = 232
-SAMPLE = True
+SAMPLE = False
 ROWS_PR_ITERATION = 20000
 NEW_SIZE = 100000
 BALANCE_CLASSES = True
@@ -43,53 +43,11 @@ class Set(IntEnum):
     TEST = 3
 
 
-def create_random_array(balance_classes: bool, all_classes_must_exist: bool, balance_hard: bool, types: np.ndarray, new_size: int) -> np.ndarray:
-    print("\nType counts:")
-    unique, counts = np.unique(types, return_counts=True)
-    for i in range(len(unique)):
-        print(
-            f"{list(TYPES_DICT.keys())[list(TYPES_DICT.values()).index(unique[i])]}: {counts[i]}")
-    missing_types = set(TYPES_DICT.values()) - set(unique)
-    if len(missing_types) > 0:
-        print("\nMissing types:")
-        for t in missing_types:
-            print(
-                f"{list(TYPES_DICT.keys())[list(TYPES_DICT.values()).index(t)]}")
-        if all_classes_must_exist:
-            # Raise error if hard_balance is True and there are missing types
-            raise Exception("All classes doesn't exists.")
-    if not balance_classes:
-        # Create a numpy array of the given size and set all to zeroes
-        arr = np.zeros(len(types), dtype=bool)
-        # Set the values for the three splits
-        # index 0 (inclusive) up to index "split1" (exclusive)
-        arr[0:new_size] = True
-        # Shuffle the indexes of the array and return
-        np.random.shuffle(arr)
-    else:
-        # Create a dictionary to keep track of the number of elements of each type
-        counts = {t: np.count_nonzero(types == t)
-                  for t in np.unique(types)}
-        # Calculate the number of elements to include for each type
-        type_size = min(counts.values()) if balance_hard else round(
-            new_size / len(counts))
-        print(f"\nNumber of elements to take for each type: {type_size}")
-        if new_size % len(counts) != 0:
-            warnings.warn(
-                f"\nWarning! The new_size={new_size} is not divisible by the number of {len(counts)} types.\nThe final array will contain True values less than the given new_size.\n")
-        # Create a numpy array of the given size and set all to False
-        arr = np.zeros(len(types), dtype=bool)
-        # Iterate over the types and set the corresponding indexes to True
-        for key, value in counts.items():
-            indices = [i for i, x in enumerate(types) if x == key]
-            num_elements = len(indices)
-            if num_elements < type_size:
-                arr[indices] = True
-                warnings.warn(
-                    f"Warning! type {key} has only {num_elements} elements - adding all.\nThe final array will contain True values less than the given new_size.\n")
-            else:
-                arr[np.random.choice(indices, type_size, replace=False)] = True
-        print(f"True values in the final array: {np.sum(arr)}")
+def create_random_array(size:int) -> np.ndarray:
+    # Create a numpy array of the given size and set all to zeroes
+    arr = np.arange(size)
+    # Shuffle the indexes of the array and return
+    np.random.shuffle(arr)
     return arr
 
 
@@ -107,13 +65,12 @@ def csv_split(filename: str, dirname: str = 'csv-chunks', rows_pr_iteration: int
             f'{dirname}/{i+1:0{padding}}.csv', index=False)
 
 
-def data_preparation(filename: str, new_filename: str, rows_pr_iteration: int = 20000) -> np.ndarray:
+def remove_unwanted(filename: str, new_filename: str, rows_pr_iteration: int = 20000) -> int:
     with open(filename, encoding='utf-8') as f:
         colnames = pd.DataFrame(columns=next(csv.reader(f)))
     colnames.to_csv(new_filename, mode='w', index=False)
     original_rows = 0
     retained_rows = 0
-    type_arr = np.empty(0, dtype=int)
     with pd.read_csv(filename, encoding='utf-8', chunksize=rows_pr_iteration, lineterminator='\n') as reader:
         for chunk in tqdm(reader, desc='remove missing/incorrect values', unit='rows encountered', unit_scale=rows_pr_iteration, colour=TQDM_COLOR):
             original_rows += chunk.shape[0]
@@ -121,58 +78,41 @@ def data_preparation(filename: str, new_filename: str, rows_pr_iteration: int = 
             chunk = chunk.dropna(subset=['content'])
             # Remove rows where type is not one of the specified values:
             chunk = chunk[chunk['type'].isin(TYPES_DICT)]
-            retained_rows += chunk.shape[0]
+            # Set unique index for each row:
+            chunk.iloc[:,0] = chunk.iloc[:,0].reset_index(
+                drop=True).index + retained_rows
             chunk.to_csv(new_filename, mode='a', header=None, index=False)
-            # Append the 'type' column to the type_array
-            # Append the value of the 'type' column to the type_array
-            type_arr = np.append(
-                type_arr, chunk['type'].map(TYPES_DICT))
+            retained_rows += chunk.shape[0]
     print(
         f"Removed rows: {original_rows-retained_rows}\n(rows before: {original_rows}, rows after (retained): {retained_rows})")
-    return type_arr
-
-
-def create_dataset(types: np.ndarray, new_size: int, old_filename: str, new_filename: str, rows_pr_iteration: int = 20000, balance_classes: bool = False, all_classes_must_exist: bool = False, balance_hard: bool = False):
-    old_size = len(types)
-    rows_pr_iteration = min(rows_pr_iteration, old_size)
-    new_size = min(new_size, old_size)
-    arr = create_random_array(
-        balance_classes=balance_classes, all_classes_must_exist=all_classes_must_exist, balance_hard=balance_hard, types=types, new_size=new_size)
-    # Write the header row to the new files:
-    with open(old_filename, encoding='utf-8') as f:
-        colnames = pd.DataFrame(columns=next(csv.reader(f)))
-    colnames.to_csv(new_filename, mode='w', index=False)
-    # Loop through data in chunks and append to the right dataset:
-    start = 0
-    with pd.read_csv(old_filename, encoding='utf-8', chunksize=rows_pr_iteration, lineterminator='\n') as reader:
-        for chunk in tqdm(reader, desc='creating dataset', total=int(old_size/rows_pr_iteration), unit='rows encountered', unit_scale=rows_pr_iteration, colour=TQDM_COLOR):
-            # Get the amount of the split array so it matches the size of the chunk.
-            end = min(start + rows_pr_iteration, old_size)
-            chunk_split = arr[start:end]
-            start += chunk.shape[0]
-            # Select the values from the chunk for the dataset:
-            rows = chunk[chunk_split == True]
-            if rows.shape[0] > 0:
-                rows.to_csv(new_filename, mode='a', header=None, index=False)
-    print("Dataset created!")
-
+    return retained_rows
 
 def read_rows(filename: str, idx: int, num: int = 1) -> int:
     return pd.read_csv(filename, encoding='utf-8', lineterminator='\n', skiprows=idx, nrows=num)
 
+def create_dataset(size:int, old_filename: str, new_filename: str, rows_pr_iteration: int = 20000):
+    rows_pr_iteration = min(rows_pr_iteration, size)
+    arr = create_random_array(size=size)
+    # Write the header row to the new files:
+    with open(old_filename, encoding='utf-8') as f:
+        colnames = pd.DataFrame(columns=next(csv.reader(f)))
+    colnames.to_csv(new_filename, mode='w', index=False)
+    # Loop through cleaned dataset and take out rows corresponding to randomly created array:
+    for a in tqdm(arr, desc='creating dataset', unit='rows encountered', unit_scale=rows_pr_iteration, colour=TQDM_COLOR):
+        read_rows(old_filename, a, 1).to_csv(new_filename, mode='a', header=None, index=False)
 
-def run(sample: bool = True, rows_pr_iteration: int = ROWS_PR_ITERATION, new_size: int = 0, balance_classes: bool = False, all_classes_must_exist: bool = False, balance_hard: bool = False):
+
+def run(sample: bool = True, rows_pr_iteration: int = ROWS_PR_ITERATION, new_size: int = 0):
     if sample:
         path = "../datasets/sample/"
     else:
         path = "../datasets/large/"
     create_directory(path)
-    types = data_preparation(filename=path+"raw.csv",
+    size = remove_unwanted(filename=path+"raw.csv",
                              new_filename=path+"cleaned.csv", rows_pr_iteration=rows_pr_iteration)
-    create_dataset(types=types, new_size=new_size, old_filename=path+"cleaned.csv",
-                   new_filename=path+"dataset.csv", rows_pr_iteration=rows_pr_iteration, balance_classes=balance_classes, all_classes_must_exist=all_classes_must_exist, balance_hard=balance_hard)
+    create_dataset(size=size, old_filename=path+"cleaned.csv",
+                   new_filename=path+"dataset.csv", rows_pr_iteration=rows_pr_iteration)
 
 
 if __name__ == '__main__':
-    run(sample=SAMPLE, rows_pr_iteration=ROWS_PR_ITERATION, new_size=NEW_SIZE,
-        balance_classes=BALANCE_CLASSES, all_classes_must_exist=ALL_CLASSES_MUST_EXIST, balance_hard=BALANCE_HARD)
+    run(sample=SAMPLE, rows_pr_iteration=20)
