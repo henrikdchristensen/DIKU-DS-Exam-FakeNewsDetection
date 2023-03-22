60da1669ab7979165bf4a918764f6fb340ac163f
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-22 17:04:30 +0100

    add statistics for h5 files

--- src/filehandling.py
+++ src/filehandling.py
@@ -6,12 +6,13 @@ import os
 import pandas as pd
 from tqdm import tqdm
 import h5py
+from collections import Counter
 
 TQDM_COLOR = 'magenta'
-TYPES = ['fake', 'conspiracy', 'junksci', 'hate', 'unreliable', 'bias', 
-              'satire', 'state', 'reliable', 'clickbait', 'political']
-SAMPLE = False
-ROWS_PR_ITERATION = 20000
+TYPES = [b'fake', b'conspiracy', b'junksci', b'hate', b'unreliable', b'bias', 
+            b'satire', b'state', b'reliable', b'clickbait', b'political']
+SAMPLE = True
+ROWS_PR_ITERATION = 20
 FILE_SIZE = 10000
 PADDING = 3
 
@@ -60,33 +61,19 @@ def create_empty_string_array(cols: int) -> np.ndarray:
     return arr
 
 
-def clean_chunk(chunk: pd.DataFrame, idx_start: int) -> Tuple[pd.DataFrame, pd.DataFrame]:
-    dropped_rows = chunk.copy()
-    # Remove rows where type is not one of the specified values:
-    chunk = chunk[chunk['type'].isin(TYPES)]
-    # Save the dropped rows in a separate dataframe:
-    dropped_rows = dropped_rows[~dropped_rows['type'].isin(TYPES)]
-    # Save the dropped rows with empty content:
-    dropped_rows = pd.concat([dropped_rows, chunk[chunk['content'].isna()]])
-    # Drop rows with empty content column:
-    chunk = chunk.dropna(subset=['content'])
-    # Set unique index for each row:
-    chunk['id'] = chunk['id'].reset_index(drop=True).index + idx_start
-    # Remove index column:
-    chunk = chunk.drop(chunk.columns[[0]], axis=1)
-    dropped_rows = dropped_rows.drop(dropped_rows.columns[[0]], axis=1)
-    return chunk, dropped_rows
-
-
-def decode_h5_dataset(data):
+def decode_2d_array(data: np.ndarray) -> np.ndarray:
     str_data = []
     for d in data:
         str_data.append([s.decode('utf-8') for s in d])
     return str_data
 
 
-def csv_to_h5(csv_filename: str, hdf_filename: str):
-    with h5py.File(hdf_filename, 'w') as store, h5py.File(hdf_filename[:-3]+'_dropped.h5', 'w') as dropped_store:
+def decode_1d_array(data: np.ndarray) -> np.ndarray:
+    return [s.decode('utf-8') for s in data]
+
+
+def csv_to_h5(csv_filename: str, h5_filename: str):
+    with h5py.File(h5_filename, 'w') as store:
         with open(csv_filename, encoding='utf-8') as f:
             colnames = next(csv.reader(f))
         # Remove first coloumn (unnamed), which is not used:
@@ -94,42 +81,97 @@ def csv_to_h5(csv_filename: str, hdf_filename: str):
         cols = len(colnames)
         # Create dataset:
         data_set = store.create_dataset('data', data=create_empty_string_array(cols), maxshape=(None, cols), dtype=h5py.string_dtype(encoding='utf-8'))
-        dropped_set = dropped_store.create_dataset('data', data=create_empty_string_array(cols), maxshape=(None, cols), dtype=h5py.string_dtype(encoding='utf-8'))
         # Set the header row:
         data_set[0] = colnames
-        dropped_set[0] = colnames
         # Read the rest of the rows and assign to dataset:
-        original_rows = retained_rows = dropped_rows = 0
+        rows = 0
         for chunk in tqdm(pd.read_csv(csv_filename, encoding='utf-8', dtype=str, chunksize=ROWS_PR_ITERATION, lineterminator='\n'),
                       desc='csv to h5', unit='rows', unit_scale=ROWS_PR_ITERATION, colour=TQDM_COLOR):
-            original_rows += chunk.shape[0]
-            retained, dropped = clean_chunk(chunk, retained_rows)
-            # Append processed data to files:
-            retained_rows += retained.shape[0]
-            dropped_rows += dropped.shape[0]
-            data_set.resize((retained_rows+1, cols))
-            dropped_set.resize((dropped_rows+1, cols))
+            # Remove first column (unnamed), which is not used:
+            chunk = chunk.drop(chunk.columns[[0]], axis=1)
+            # Set unique index for each row and remove index column:
+            chunk[chunk.columns[0]] = range(rows, rows+chunk.shape[0])
+            # Resizing dataset:
+            rows += chunk.shape[0]
+            data_set.resize((rows+1, cols))
             # Check if data is empty - if not, append to dataset:
-            if len(retained) > 0:
-                data_set[-len(retained):] = retained.astype(str)
-            if len(dropped) > 0:
-                dropped_set[-len(dropped):] = dropped.astype(str)
-        print(f"Original rows: {original_rows}, retained rows: {retained_rows}, dropped rows: {dropped_rows}")
+            if len(chunk) > 0:
+                data_set[-len(chunk):] = chunk.astype(str)
+
+    
+def remove_unwanted_rows(data_filename: str, retained_filename: str, removed_filename: str):
+    with h5py.File(data_filename, 'r') as data_store, h5py.File(retained_filename, 'w') as retained_store, h5py.File(removed_filename, 'w') as removed_store:
+        # Create dataset:
+        header = data_store['data'][0]
+        rows, cols = data_store['data'].shape
+        retained_set = retained_store.create_dataset('data', data=create_empty_string_array(cols), maxshape=(None, cols), dtype=h5py.string_dtype(encoding='utf-8'))
+        removed_set = removed_store.create_dataset('data', data=create_empty_string_array(cols), maxshape=(None, cols), dtype=h5py.string_dtype(encoding='utf-8'))
+        retained_set[0] = header
+        removed_set[0] = header
+        # Initialize counters:
+        retained_rows = faulty_content_rows = faulty_type_rows = 0
+        for i in tqdm(range(1, rows, ROWS_PR_ITERATION), desc=f'remove unwanted rows', unit='rows', unit_scale=ROWS_PR_ITERATION, colour=TQDM_COLOR):
+            chunk = data_store['data'][i:i+ROWS_PR_ITERATION]
+            decoded = decode_1d_array(chunk[:,4])
+            # Remove rows with empty content and incorrect/missing types:
+            mask_content = np.logical_and(chunk[:,4] != b'nan', ~np.char.startswith(decoded, 'ERROR'))
+            mask_type = np.isin(chunk[:, 2], TYPES)
+            mask = np.logical_and(mask_content, mask_type)
+            retained_chunk = chunk[mask]
+            removed_chunk = chunk[~mask]
+            # Update counters:
+            retained_rows += np.sum(mask)
+            faulty_content_rows += np.sum(~mask_content)
+            faulty_type_rows += np.sum(~mask_type)
+            if retained_chunk.shape[0] > 0:
+                retained_set.resize(retained_set.shape[0] + retained_chunk.shape[0], axis=0)
+                retained_set[-retained_chunk.shape[0]:] = retained_chunk
+            if removed_chunk.shape[0] > 0:
+                removed_set.resize(removed_set.shape[0] + removed_chunk.shape[0], axis=0)
+                removed_set[-removed_chunk.shape[0]:] = removed_chunk
+    print(f"Original rows: {rows-1}, retained rows: {retained_rows}, rows with faulty content: {faulty_content_rows}, rows with faulty type: {faulty_type_rows}")
+
+
+def statistics(*h5_filenames: str):
+    # Initialize counters:
+    total_rows = 0
+    total_cols = 0
+    domains_counter = Counter()
+    types_counter = Counter()
+    # Iterate over all files:
+    for h5_filename in h5_filenames:
+        with h5py.File(h5_filename, 'r') as data_store:
+            rows, cols = data_store['data'].shape
+            total_rows += rows - 1
+            total_cols = cols
+            for i in tqdm(range(1, rows, ROWS_PR_ITERATION), desc=f'processing {h5_filename}', unit='rows', colour=TQDM_COLOR):
+                chunk = data_store['data'][i:i+ROWS_PR_ITERATION]
+                # Decode data:
+                domains = decode_1d_array(chunk[:,1])
+                types = decode_1d_array(chunk[:,2])
+                # Update counters:
+                domains_counter.update(domains)
+                types_counter.update(types)
+    # Print statistics:
+    print(f"Number of rows: {total_rows}, number of columns: {total_cols}")
+    print(f"Number of different domains: {dict(domains_counter)}")
+    print(f"Number of different types: {dict(types_counter)}")
+
      
-            
-def h5_to_csv(hdf_filename: str, csv_filename: str):
-    with h5py.File(hdf_filename, 'r') as read:
+def h5_to_csv(h5_filename: str, csv_filename: str):
+    with h5py.File(h5_filename, 'r') as read:
+        rows = read['data'].shape[0]
         # Save the header row to CSV
-        header = read['data'][0]
-        header = [s.decode('utf-8') for s in header]
-        pd.DataFrame([header]).to_csv(csv_filename, mode='w', header=None, index=False)
+        pd.DataFrame([decode_1d_array(read['data'][0])]).to_csv(csv_filename, mode='w', header=None, index=False)
         # Loop through the rest of the data and save it to CSV
-        for i in tqdm(range(1, read['data'].shape[0], ROWS_PR_ITERATION),
+        for i in tqdm(range(1, rows, ROWS_PR_ITERATION),
                       desc='h5 to csv', unit='rows', unit_scale=ROWS_PR_ITERATION, colour=TQDM_COLOR):
             # Get the data from the HDF5 file
             data = read['data'][i:i+ROWS_PR_ITERATION]
+            # Decode the data:
+            data = decode_2d_array(data)
             # Save the data to CSV:
-            pd.DataFrame(decode_h5_dataset(data)).to_csv(csv_filename, mode='a', header=None, index=False)
+            pd.DataFrame(data).to_csv(csv_filename, mode='a', header=None, index=False)
 
 
 def shuffle_h5(old_filename: str, new_filename: str):
@@ -151,10 +193,13 @@ def shuffle_h5(old_filename: str, new_filename: str):
 
 def run(sample: bool):
     path = "../datasets/sample/" if sample else "../datasets/large/"
-    csv_to_h5(csv_filename=path+"raw.csv", hdf_filename=path+"raw.h5")
-    shuffle_h5(old_filename=path+"raw.h5", new_filename=path+"shuffled.h5")
-    h5_to_csv(hdf_filename=path+"shuffled.h5", csv_filename=path+"dataset.csv")
-    
+    csv_to_h5(csv_filename=path+"raw.csv", h5_filename=path+"raw.h5")
+    statistics(path+"raw.h5")
+    remove_unwanted_rows(data_filename=path+"raw.h5", retained_filename=path+"retained.h5", removed_filename=path+"removed.h5")
+    shuffle_h5(old_filename=path+"retained.h5", new_filename=path+"retained_shuffled.h5")
+    h5_to_csv(h5_filename=path+"retained.h5", csv_filename=path+"retained.csv")
+    h5_to_csv(h5_filename=path+"retained_shuffled.h5", csv_filename=path+"retained_shuffled.csv")
+    h5_to_csv(h5_filename=path+"removed.h5", csv_filename=path+"removed.csv")
 
 if __name__ == '__main__':
     run(sample=SAMPLE)
