2ea913ba095f9988aecdb03c51ad12ad8a76e2c0
Henrik Christensen <henrikdchristensen@gmail.com>
2023-03-28 11:12:53 +0200

    make pipeline for liar dataset

---
+++ src/liar.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+import pipeline as pp
+import pandas as pd
+from textblob import TextBlob
+import filehandling as fh
+
+headers = ['id', 'label', 'statement', 'subjects', 'speaker', 'speaker_job', 'state_info', 
+           'party', 'barely_true', 'false', 'half_true', 'mostly_true', 'pants_on_fire', 'context']
+
+
+def combine_tsv_files(files: list, new_file: str):
+    fh.remove_file(new_file)
+    df = pd.DataFrame(columns=headers)
+    for file in files:
+        df = pd.concat([df, pd.read_csv(file, delimiter='\t')])
+    df.to_csv(new_file, sep='\t', index=False)
+
+def set_headers(file: str, new_file: str):
+    df = pd.read_csv(file, delimiter='\t', header=None)
+    df.columns = headers
+    df.to_csv(new_file, sep='\t', index=False)
+
+    
+class Clean_id(pp.FunctionApplier):
+    def function_to_apply(self, id):
+        return id.split('.')[0]
+    
+def clean_dataset(file, new_file):
+    #stopwords_lst = stopwords.words('english')
+    pp.apply_pipeline(file, [
+        # Binary labels
+        #(pp.Binary_labels(), 'type', 'type_binary'),
+        # Clean content
+        (Clean_id(), 'id', 'id'),
+        #(pp.Clean_data(), 'statement', 'statement_cleaned'),
+        #(pp.Tokenizer(), 'statement_cleaned'),
+        #(pp.Remove_stopwords(stopwords_lst), 'statement_cleaned'),
+        #(pp.Stem(), "statement_cleaned"),
+        #(pp.Combine_Content(), 'statement_cleaned', 'statement_combined'),
+        #(pp.Sentence_analysis(), 'statement_combined', 'sentence_analysis'),
+    ],
+        new_file=new_file,
+        progress_bar=True,
+        delimiter='\t',
+    )
+
+
+if __name__ == '__main__':
+    set_headers('../datasets/liar_dataset/raw/train.tsv', '../datasets//liar_dataset/cleaned/train.tsv')
+    set_headers('../datasets/liar_dataset/raw/valid.tsv', '../datasets//liar_dataset/cleaned/valid.tsv')
+    set_headers('../datasets/liar_dataset/raw/test.tsv', '../datasets//liar_dataset/cleaned/test.tsv')
+    combine_tsv_files(['../datasets/liar_dataset/cleaned/train.tsv', '../datasets/liar_dataset/cleaned/valid.tsv', '../datasets/liar_dataset/cleaned/test.tsv'], '../datasets/liar_dataset/cleaned/combined.tsv')
+    clean_dataset('../datasets/liar_dataset/cleaned/combined.tsv', '../datasets/liar_dataset/cleaned/combined_cleaned.tsv')
